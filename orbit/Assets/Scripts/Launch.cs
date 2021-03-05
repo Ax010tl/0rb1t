ea08dda99489005ae23475140bb20a42e543f96e
louloubadillo <lulu.badillo.csm@gmail.com>
2021-03-05 14:18:29 -0600

    rocket rotates

--- orbit/Assets/Scripts/Launch.cs
+++ orbit/Assets/Scripts/Launch.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/**
+* Permitir que el cohete pueda ser disparado al arrastrar el mouse
+Lulú Badillo
+03/03/2021
+*/
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +16,7 @@ public class Launch : MonoBehaviour
     public Transform pivot; 
     //Rango en el que se puede arrastrar el cohete
     public float dragRange;
+    //Máxima velocidad que puede alcanzar el cohete
     [SerializeField] public float maxVel; 
     Rigidbody2D rb;
 
@@ -21,33 +28,46 @@ public class Launch : MonoBehaviour
     }
 
     bool canDrag = true; 
+    //Distancia
     Vector3 dis; 
     void OnMouseDrag()
     {
+        //Si ya se lanzó 
         if(!canDrag)
             return; 
 
         var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
         dis = pos - pivot.position; 
         dis.z = 0; //Es 2D, entonces hay que quitarle el componente z
+        
+        float angleRadians = (float) Math.Atan2(dis.x, -dis.y);
+        float angleDegrees = angleRadians * (180/ (float) Math.PI);
+
         if(dis.magnitude > dragRange) //si se arrastra fuera del rango definido
         {
             dis = dis.normalized * dragRange; 
         }
         transform.position = dis + pivot.position;
+        transform.rotation = Quaternion.Euler(0, 0, angleDegrees-42.381f);
+
     }
 
     void OnMouseUp()
     {
+        //Si ya se lanzó
         if(!canDrag)
             return; 
         
-        canDrag = false; 
+        //Si todavía no se ha lanzado
+        canDrag = false; //Ya no se puede volver a lanzar
         rb.bodyType = RigidbodyType2D.Dynamic; 
+        //Queremos que funcione como una resortera, entonces debe salir disparado hacia el componente opuesto al que se arrastra
         dis.y *= -1; 
+        dis.x *= -1; 
+        //La velocidad cambia dependiendo de qué tanto se arrastró en x,y
         rb.velocity = dis.normalized * maxVel * dis.magnitude / dragRange; 
     }
-    // Update is called once per frame
+   
     void Update()
     {
         
