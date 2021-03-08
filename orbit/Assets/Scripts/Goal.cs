390b16367d9f1c02c32c51babc9563306a0d74dd
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-03-08 16:29:38 -0600

    Añadir pantallaz kul

--- orbit/Assets/Scripts/Goal.cs
+++ orbit/Assets/Scripts/Goal.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
@@ -21,9 +22,8 @@ public class Goal : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.tag == "Player"){
             numberOfRevolutions++;
-            // print(numberOfRevolutions)
             if(numberOfRevolutions==minRevolutions){
-                print("Felicidades!!");
+                SceneManager.LoadScene("FunFact");
             }
 
         }
