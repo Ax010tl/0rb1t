471b16838440cecb7df629445a926311533e15c7
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-03-10 15:39:24 -0600

    Add circular motion script for obstacles

---
+++ orbit/Assets/Scripts/CircularMotion.cs
@@ -0,0 +1,52 @@
+﻿/*
+Script for moving objects around another one constantly (circular motion).
+No need to worry about position :)
+Valeria Pineda
+09/03/2021
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMotion : MonoBehaviour
+{
+    [SerializeField] GameObject planet;
+    public float radius;
+    public float speed;
+    public bool loopy;      // when enabled, orbit is elliptical
+    public int direction;   // greater/equal than zero goes right, negative goes left
+    float t;
+    float offset;           // for elliptical orbit
+
+    private void Start()
+    {
+        // place object above planet, depending on radius
+        transform.position = new Vector3(planet.transform.position.x, planet.transform.position.y + radius, 0);
+
+        // random elliptical orbit
+        System.Random rd = new System.Random();
+        if (loopy)
+        {
+            offset = rd.Next(30, 60);
+        }
+
+        if (direction >= 0)
+        {
+            speed *= -1;
+        }
+    }
+
+    void Update()
+    {
+        // trig :D
+        t += Time.deltaTime * speed;
+
+        float x = (float)Math.Cos(t + offset) * radius;
+        float y = (float)Math.Sin(t - offset) * radius;
+        float z = 0;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
