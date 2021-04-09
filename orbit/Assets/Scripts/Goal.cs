9941972dc476225d47aaaea9f1cadd5b4680dc0e
louloubadillo <lulu.badillo.csm@gmail.com>
2021-04-08 21:15:32 -0500

    closes #2, progress on scoring system

--- orbit/Assets/Scripts/Goal.cs
+++ orbit/Assets/Scripts/Goal.cs
@@ -2,24 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
-    [SerializeField] int numberOfRevolutions;
+    int numberOfRevolutions;
     [SerializeField] int minRevolutions;
+    [SerializeField] Text scoreText;
+    int score;
     // Start is called before the first frame update
     void Start()
     {
         numberOfRevolutions = 0;
+        score = 0; 
+        //print starting score
+        scoreText.text = "Puntaje: " + score.ToString();
+    }
+
+    //Keep score
+    private void HandleScore ()
+    {
+        //Change text to new score
+        scoreText.text = "Puntaje: " + score.ToString(); 
+
+        if(score < 0){
+            SceneManager.LoadScene("End");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collider){
-        if(collider.tag == "Player"){
+        //We need to add points if it manages two complete orbits
+        if(collider.tag == "Limit"){
             numberOfRevolutions++;
             if(numberOfRevolutions == minRevolutions){
+                score++;
+                HandleScore ();
                 SceneManager.LoadScene("FunFact");
             }
-
+        }
+        if(collider.tag == "Planet")
+        {
+            score--;
+            HandleScore();
         }
     }
 
