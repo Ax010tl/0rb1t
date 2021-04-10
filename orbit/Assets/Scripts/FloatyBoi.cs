fa7448c9ec94b89686cc58e8e3076aa84581ba29
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-04-10 00:29:02 -0500

    close #1

---
+++ orbit/Assets/Scripts/FloatyBoi.cs
@@ -0,0 +1,31 @@
+﻿/*
+Move objects so it looks like they're levitating.
+Valeria Pineda
+07/04/2021
+*/
+
+using UnityEngine;
+
+public class FloatyBoi : MonoBehaviour
+{
+    float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = -0.002f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y + speed,
+            0
+        );
+
+        if (transform.position.y <= -0.2 || transform.position.y >= 0.2f)
+            speed *= -1;
+    }
+}
