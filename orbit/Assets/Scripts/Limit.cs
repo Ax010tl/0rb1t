a23ba6632219c8fac3d402fb79146a1f3eeb6472
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-04-10 17:07:59 -0500

    add random sprites
    Co-authored-by: Eduardo Villalpando Mello <edvilme@users.noreply.github.com>
    Co-authored-by: Lourdes Badillo <louloubadillo@users.noreply.github.com>

--- orbit/Assets/Scripts/Limit.cs
+++ orbit/Assets/Scripts/Limit.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿/*
+If the rocket exceeds the game limits, you lose a life. 
+Valeria Pineda
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
@@ -14,8 +18,17 @@ public class Limit : MonoBehaviour
     void OnTriggerExit2D(Collider2D col) {
         if(col.tag == "Player")
         {
+            // Subtract life
             print("-1 vida :(");
-            SceneManager.LoadScene("Level1");
+            PlayerPrefs.SetInt("lives", PlayerPrefs.GetInt("lives")-1);
+            print(PlayerPrefs.GetInt("lives"));
+
+            // Return rocket to start position
+            GameObject rocket = GameObject.FindWithTag("Player");
+            rocket.transform.position = new Vector3(-0.03000032f, -3.17f, 0);
+            rocket.transform.rotation = Quaternion.Euler(0, 0, -42.381f);
+
+            // SceneManager.LoadScene("Level1");
         }
     }
 }
---
+++ orbit/Assets/Scripts/SaveData.cs
@@ -0,0 +1,57 @@
+﻿/*
+10/04/2021
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour
+{
+    public Text livesText;
+    public Text scoreText;
+    public Text levelText;
+    public static int lives; 
+    public static int score; 
+    public static int level; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Change text to show level 
+        levelText.text = "Nivel: " + PlayerPrefs.GetInt("level"); 
+        // Change text to show score
+        scoreText.text = "Puntaje: " + PlayerPrefs.GetInt("score"); 
+        //C hange text to show lives
+        livesText.text = "Vidas: " + PlayerPrefs.GetInt("lives"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int getLives(){
+        return lives; 
+    }
+
+    int getScore(){
+        return score; 
+    }
+
+    int getLevel(){
+        return level; 
+    }
+
+    void setLives(int num){
+        lives = num; 
+    }
+
+    void setScore(int num){
+        score = num;
+    }
+
+    void setLevel(int num){
+        level = num;
+    }
+}
